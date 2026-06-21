f18a09bed84bc79abe2d33f9ac0205cf673c35d4
Doscharl <dosso.choilio@gmail.com>
2026-06-21 22:11:07 +0000

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,16 @@
-/** @type {import('@capacitor/cli').CapacitorConfig} */
-const config = {
-  appId: 'com.dosco.bataille.etoiles',
-  appName: 'DOSCO',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-  android: {
-    allowMixedContent: true,
+{
+  "appId": "com.dosco.bataille.etoiles",
+  "appName": "DOSCO",
+  "webDir": "dist",
+  "bundledWebRuntime": false,
+  "android": {
+    "allowMixedContent": true
   },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 1500,
-      backgroundColor: '#03040a',
-      showSpinner: false,
-    },
-  },
-};
-
-module.exports = config;
+  "plugins": {
+    "SplashScreen": {
+      "launchShowDuration": 1500,
+      "backgroundColor": "#03040a",
+      "showSpinner": false
+    }
+  }
+}
